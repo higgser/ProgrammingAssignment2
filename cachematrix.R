4726ee445160c99f3d3047dadf9923ecf634f71a
higgser <manuel.walser@googlemail.com>
2014-12-17 07:56:25 +0100

    implemented cachematrix functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Functions to extend and work with matrices that have an internal cache.
+## The cache can store results of expensive calculations, e.g. the inverse
+## of the matrix.
 
-## Write a short comment describing this function
+## makeCacheMatrix(x) wraps the given matrix 'x' in a new object that has an
+## internal cache. The object has setter and getter functions for the matrix
+## and the inverse of the matrix. Setting a new matrix clears the cache.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    inv <- NULL
+    set <- function(y) {
+        x <<- y
+        inv <<- NULL
+    }
+    get <- function() { x }
+    setinv <- function(inverse) { inv <<- inverse }
+    getinv <- function() { inv }
+    list(set = set, get = get, setinv = setinv, getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve(x) returns the inverse of tne cacheMatrix 'x . On the first call,
+## the inverse matrix is calculated and stored in the cache. Subsequent calls
+## return the cached value.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    inv <- x$getinv()
+    if (!is.null(inv)) {
+        return(inv)
+    }
+    matrix <- x$get()
+    inv <- solve(matrix)
+    x$setinv(inv)
+    inv
 }
